91fa4df9b8a9e0ff78c247e9e1c2f37329b81716
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-26 18:14:53 +0900

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.psql'),
+        'NAME': os.path.join(BASE_DIR, '/var/run/postgresql/.s.PGSQL.5432'),
     }
 }
 
