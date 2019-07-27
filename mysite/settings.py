274d6d53652b873f1f5426cf3d2171f3b0b2f662
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-27 15:50:25 +0900

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,7 +154,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'build/static'),
+    os.path.join(BASE_DIR, 'build/static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
