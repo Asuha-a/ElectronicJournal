55e9178c8f75e98726fd64d702a3950d96e11e98
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-27 20:16:55 +0900

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,7 +61,8 @@ JWT_AUTH = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
