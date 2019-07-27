863fbd998a9082792e5859e02423c0ebb2a172aa
Asuha-a <kobayashi.91.1775@docomo.ne.jp>
2019-07-27 12:34:22 +0900

    deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,10 +16,6 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'jr2^#efv^09%0$jb=if*ba#lhq-%1_!pz4*(-8ao3avsi$8np3'
 
